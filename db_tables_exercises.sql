6f5f2dcd7bf915d6231555187fb22e6318d68a80
MigashaneVictoire <Migashanevictoire@gmail.com>
2023-05-06 09:26:40 -0500

    finalize my solutions and team solutions to to get final result

--- db_tables_exercises.sql
+++ db_tables_exercises.sql
@@ -1,38 +1,61 @@
--- 3. See all available databases
+-- 3. List all the databases
 SHOW DATABASES; 
 
--- 4. Avoid using the dot (.) notation by using USE
+-- 4. Write the SQL code necessary to use the albums_db database
 USE albums_db;
 
-# 5. Show the database that we are using
-SELECT database();
-
--- 6. See all available table
-SHOW TABLES; 
-
-# 7. Switch to a different database
-USE employees;
-
-# 8. Showe the new current database
+# 5. Show the currently selected database
 SELECT DATABASE();
 
-# 9. Show the tables in my current database
+-- 6. List all tables in the database
+SHOW TABLES; 
+
+# 7. Write the SQL code to switch the employees database
+USE employees;
+
+# 8. Show the currently selected database
+SELECT DATABASE();
+
+# 9. List all the tables in the database
 SHOW TABLES;
 
-# shoe how the table was created
-SHOW CREATE TABLE employees;
--- int, date, varchar(14), varchar(16), enum("M", "F"), date
+# 10. Explore the employees table. What different data types are present on this table?
+-- Which table(s) do you think contain a numeric type column?
 
-DESCRIBE employees; -- used this syntax to solve question 11 to 13
-# 11. Which table(s) do you think contain a numeric type column? (Write this question and your answer in a comment)
+USE employees;
+SHOW CREATE TABLE employees;
+
+-- Can also use:
+DESCRIBE employees;
+
+-- Better enswer:
+SELECT COLUMN_NAME, DATA_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE table_schema = 'employees'
+	AND table_name = 'employees';
+
+-- 'emp_no','int'
+-- 'birth_date','date'
+-- 'first_name','varchar'
+-- 'last_name','varchar'
+-- 'gender','enum'
+-- 'hire_date','date'
+
+# 11. Which table(s) do you think contain a numeric type column? 
+-- (Write this question and your answer in a comment)
+
 /* These table contains numeric datatypes:
-dept_emp
-dept_manager
-salaries
+'departments'
+'dept_emp'
+'dept_manager'
+'employees'
+'salaries'
+'titles'
+
 */
--- class responce: all the tables
 
 # 12. Which table(s) do you think contain a string type column? (Write this question and your answer in a comment)
+    
 /*
 These table contains numeric datatypes:
 departments
@@ -42,7 +65,7 @@ employees
 titles
 */
 
-# Which table(s) do you think contain a date type column? (Write this question and your answer in a comment)
+# 13. Which table(s) do you think contain a date type column? (Write this question and your answer in a comment)
 /*
 These table contains numeric datatypes:
 dept_emp
@@ -52,14 +75,22 @@ salaries
 titles
 */
 
-# What is the relationship between the employees and the departments tables? (Write this question and your answer in a comment)
-/*
-departements has impormation about the department but employees has a detailed column description for employees
-*/
--- class responce: No direct relationship
+# 14. What is the relationship between the employees and the departments tables? (Write this question and your answer in a comment)
+-- No direct relationship
 
 # Show the SQL that created the dept_manager table. Write the SQL it takes to show this as your exercise solution.
--- SHOW CREATE TABLE dept_manager;
+SHOW CREATE TABLE dept_manager;
 
-
+/*
+CREATE TABLE `dept_manager` (
+  `emp_no` int NOT NULL,
+  `dept_no` char(4) NOT NULL,
+  `from_date` date NOT NULL,
+  `to_date` date NOT NULL,
+  PRIMARY KEY (`emp_no`,`dept_no`),
+  KEY `dept_no` (`dept_no`),
+  CONSTRAINT `dept_manager_ibfk_1` FOREIGN KEY (`emp_no`) REFERENCES `employees` (`emp_no`) ON DELETE CASCADE ON UPDATE RESTRICT,
+  CONSTRAINT `dept_manager_ibfk_2` FOREIGN KEY (`dept_no`) REFERENCES `departments` (`dept_no`) ON DELETE CASCADE ON UPDATE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+*/
 
